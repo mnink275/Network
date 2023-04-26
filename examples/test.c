5781235d6cbe3f2eb1c841de6668f51d0f54726a
Ink WSL <nmingraripov@gmail.com>
2023-04-26 21:10:26 +0300

    test

--- examples/test.c
+++ examples/test.c
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <unistd.h>
 
-int main(void) {
-    int a[10];
-    std::cout << sizeof a; 
+int main() {
+    const size_t len = 30;
+    char name[len];
+    gethostname(name, len);
+    for (int i = 0; i < len; ++i) {
+        std::cout << name[i];
+    }
+    std::cout << "\n";
 
     return 0;
 }
